e1661ec2d1ebcaf1e2c7a7e4a414f313499373d0
martygarrix <69579056+martygarrix@users.noreply.github.com>
2020-08-12 15:22:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,16 @@
 
 
 ///Edit Under This only
-
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB1a0zN_mmucaM5ghpT6aGyNuXCZsKeKlg",
+  authDomain: "instagram-21dc8.firebaseapp.com",
+  databaseURL: "https://instagram-21dc8.firebaseio.com",
+  projectId: "instagram-21dc8",
+  storageBucket: "instagram-21dc8.appspot.com",
+  messagingSenderId: "646557260563",
+  appId: "1:646557260563:web:8f2bc39dcbb32243e7d8e6",
+  measurementId: "G-PRZDRHY1TR"
 };
-
      
  
 ////Do Not Edit Below This
